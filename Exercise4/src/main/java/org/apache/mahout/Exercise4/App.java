4ba78c51130436871de771d717232ffdfb69838c
Selina Klees <selina.klees@o2online.de>
2017-11-20 17:50:24 +0100

    eclipse hello world

---
+++ Exercise4/src/main/java/org/apache/mahout/Exercise4/App.java
@@ -0,0 +1,13 @@
+package org.apache.mahout.Exercise4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
